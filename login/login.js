6e8e19ddac26c8eb3a3ac4f277ad5a79d226a171
64070014Fluk <64070014@it.kmitl.ac.th>
2023-02-27 00:18:01 +0700

    add Login & Signup page
    vue ✗
    card ✓

---
+++ login/login.js
@@ -0,0 +1,12 @@
+var login = new Vue({
+    el: '#login',
+    data: {
+        users: [{
+            user_id: '',
+            user_password: '',
+            user_email: '',
+            user_fname: '',
+            user_lname: '',
+        }]
+    }
+})
