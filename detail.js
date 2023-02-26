af1e8b7c4f88ad2e4b44cfc4eeb8ac2f05e62783
64070014Fluk <64070014@it.kmitl.ac.th>
2023-02-26 22:16:11 +0700

    move vue to js file

--- detail.js
+++ detail.js
@@ -1,60 +1,59 @@
-var app = new vue({
+var app = new Vue({
     el: '#app',
     data: {
         wm_img: './img/wm.png',
         Washing_machine: [
             {
-                wm_id: 001,
+                wm_id: '001',
                 wm_brand: 'Samsung',
                 wm_model: '',
                 wm_status: 0,
                 queue_id: 0,
-                wm_img: './img/wm.png',
             },
             {
-                wm_id: 002,
+                wm_id: '002',
                 wm_brand: 'Toshiba',
                 wm_model: '',
                 wm_status: 2,
                 queue_id: 0
             },
             {
-                wm_id: 003,
+                wm_id: '003',
                 wm_brand: 'Toshiba',
                 wm_model: '',
                 wm_status: 1,
                 queue_id: 0
             },
             {
-                wm_id: 004,
+                wm_id: '004',
                 wm_brand: 'Toshiba',
                 wm_model: '',
                 wm_status: 2,
                 queue_id: 0
             },
             {
-                wm_id: 005,
+                wm_id: '005',
                 wm_brand: 'Toshiba',
                 wm_model: '',
                 wm_status: 0,
                 queue_id: 0
             },
             {
-                wm_id: 006,
+                wm_id: '006',
                 wm_brand: 'Toshiba',
                 wm_model: '',
                 wm_status: 0,
                 queue_id: 0
             },
             {
-                wm_id: 007,
+                wm_id: '007',
                 wm_brand: 'Toshiba',
                 wm_model: '',
                 wm_status: 0,
                 queue_id: 0
             },
             {
-                wm_id: 08,
+                wm_id: '008',
                 wm_brand: 'Toshiba',
                 wm_model: '',
                 wm_status: 2,
