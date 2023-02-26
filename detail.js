e84b2221e5d4a35605757d692b24403d904d7fff
64070014Fluk <64070014@it.kmitl.ac.th>
2023-02-26 21:37:16 +0700

    Draft 2
    bg ✓
    vue ✗

---
+++ detail.js
@@ -0,0 +1,66 @@
+var app = new vue({
+    el: '#app',
+    data: {
+        wm_img: './img/wm.png',
+        Washing_machine: [
+            {
+                wm_id: 001,
+                wm_brand: 'Samsung',
+                wm_model: '',
+                wm_status: 0,
+                queue_id: 0,
+                wm_img: './img/wm.png',
+            },
+            {
+                wm_id: 002,
+                wm_brand: 'Toshiba',
+                wm_model: '',
+                wm_status: 2,
+                queue_id: 0
+            },
+            {
+                wm_id: 003,
+                wm_brand: 'Toshiba',
+                wm_model: '',
+                wm_status: 1,
+                queue_id: 0
+            },
+            {
+                wm_id: 004,
+                wm_brand: 'Toshiba',
+                wm_model: '',
+                wm_status: 2,
+                queue_id: 0
+            },
+            {
+                wm_id: 005,
+                wm_brand: 'Toshiba',
+                wm_model: '',
+                wm_status: 0,
+                queue_id: 0
+            },
+            {
+                wm_id: 006,
+                wm_brand: 'Toshiba',
+                wm_model: '',
+                wm_status: 0,
+                queue_id: 0
+            },
+            {
+                wm_id: 007,
+                wm_brand: 'Toshiba',
+                wm_model: '',
+                wm_status: 0,
+                queue_id: 0
+            },
+            {
+                wm_id: 08,
+                wm_brand: 'Toshiba',
+                wm_model: '',
+                wm_status: 2,
+                queue_id: 0
+            },
+        ],
+        
+    }
+})
