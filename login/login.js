7dde824d3b10dc951e6b9762425fe2d814ab8c51
64070014Fluk <64070014@it.kmitl.ac.th>
2023-02-28 22:57:05 +0700

    login didn't show

--- login/login.js
+++ login/login.js
@@ -1,12 +1,14 @@
 var login = new Vue({
     el: '#login',
     data: {
+        show_login: true,
+        show_signup: false,
         users: [{
             user_id: '',
             user_password: '',
             user_email: '',
             user_fname: '',
             user_lname: '',
-        }]
+        }],
     }
 })
