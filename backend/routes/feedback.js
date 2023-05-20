023d670cdb181844be7c343acd45751725422dca
64070014Fluk <64070014@it.kmitl.ac.th>
2023-05-20 21:36:15 +0700

    FeedbackPage (incomplete)

--- backend/routes/feedback.js
+++ backend/routes/feedback.js
@@ -5,7 +5,7 @@ router = express.Router();
 
 const { isLoggedIn } = require('../middlewares')
 
-router.get("/feedback", isLoggedIn, async function (req, res, next) {
+router.get("/feedback", async function (req, res, next) {
 
     const conn = await pool.getConnection()
     await conn.beginTransaction();
@@ -18,19 +18,33 @@ router.get("/feedback", isLoggedIn, async function (req, res, next) {
             "SELECT * FROM users",
             [feedback[0].email]
         )
-        conn.commit()
+
         // console.log(feedback)
         return res.json({
             feedback: feedback,
             email: email,
         })
     } catch (err) {
-        conn.rollback()
         return res.send(err);
-    } finally {
-        conn.release()
     }
+});
 
+router.get("/feedback/:id", async function (req, res, next) {
+
+    const conn = await pool.getConnection()
+    await conn.beginTransaction();
+
+    try {
+        const [feedbackReader] = await conn.query(
+            "SELECT *, DATE_FORMAT(feedback_date, '%d-%m-%Y') AS feedback_date, TIME(feedback_date) AS feedback_time FROM feedback WHERE id=?",
+            [req.params.id]
+        )
+        return res.json({
+            feedbackRead:feedbackReader,
+        })
+    } catch (err) {
+        return res.send(err);
+    }
 });
 
 exports.router = router;
